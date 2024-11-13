c7e8d6fdc3f38687fdefc0e89a8efef5bcc33287
Lin Li <116258922+linli2492@users.noreply.github.com>
2024-11-13 20:43:55 +0000

    Delete QuantitativeAnalysisAndVisualization/MarketProfileVolumeProfile/database directory

--- QuantitativeAnalysisAndVisualization/MarketProfileVolumeProfile/database/db_config.py
+++
@@ -1,9 +0,0 @@
-import os 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-db_config = {"host": "localhost",
-             "database": "ficc_data",
-             "user": "postgres",
-             "password": os.getenv("DB_PASSWORD")}
